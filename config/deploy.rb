372c3651e1433f74c34b05fcd9fdbeface517ddb
Thomas von Deyen <tvdeyen@gmail.com>
2010-08-10 19:56:01 +0200

    deploy tasks

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,41 +1,25 @@
 # set the applicationname here
-set :application, ""
+set :application, "alchemy-app.com"
 
-# ssh user settings. please change to customers
-set :user, ""
-set :password, ""
-ssh_options[:port] = 12312
+set :user, { Capistrano::CLI.ui.ask("Type in the ssh username: ") }
+set :password, { Capistrano::CLI.password_prompt("Type in the password for #{user}: ") }
 
-set :database_user, "#{user}"
-set :database_password, ""
-set :database_name, "usr_#{user}_1"
-set :database_host, "localhost"
-set :database_socket, "/var/run/mysqld/mysqld.sock"
+set :scm, :git
+set :repository, "git://github.com/tvdeyen/alchemy-app.com.git"
+
+set :deploy_via, :remote_cache
+set :copy_exclude, [".svn", ".DS_Store"]
 
 # please set domain names
-role :app, ""
-role :web, ""
-role :db,  "", :primary => true
+role :app, "alchemy-app.com"
+role :web, "alchemy-app.com"
+role :db,  "alchemy-app.com", :primary => true
 
-# set the public webserver path
-set :deploy_to, "/var/www/#{user}/html/alchemy"
-
-# set the apps repository url
-set :repository_url, "http://svn.vondeyen.com/customers/#{application}"
-
-##### DO NOT CHANGE BELOW THIS LINE #########
-
-set :scm, :subversion
-set :scm_user, "capistrano"
-set :scm_password, "C4p1fy"
-set :use_sudo, false
-
-set :repository, Proc.new{ "--username #{scm_user} --password #{scm_password} #{repository_url}" }
+set :deploy_to, "/var/www/#{user}/html/webpage"
 
 before "deploy:restart", "deploy:migrate"
 
 after "deploy:setup", "alchemy:create_shared_folders"
-after "deploy:symlink", "alchemy:create_database_yml"
 after "deploy:symlink", "alchemy:symlink_folders"
 
 namespace :alchemy do
@@ -47,22 +31,6 @@ namespace :alchemy do
     run "mkdir -p #{shared_path}/cache/pictures"
   end
   
-  desc "Creates the database.yml file for server database"
-  task :create_database_yml, :roles => :app do
-    db_config = ERB.new <<-EOF
-    production:
-      adapter: mysql
-      encoding: utf8
-      database: #{database_name}
-      username: #{database_user}
-      password: #{database_password}
-      host: #{database_host}
-      socket: #{database_socket}
-    EOF
-    run "mkdir -p #{shared_path}/config" 
-    put db_config.result, "#{shared_path}/config/database.yml"
-  end
-  
   desc "Sets the symlinks for uploads and pictures cache folder"
   task :symlink_folders, :roles => :app do
     run "rm -rf #{current_path}/uploads/*"
@@ -72,15 +40,7 @@ namespace :alchemy do
     run "ln -nfs #{shared_path}/cache/pictures/ #{current_path}/public/pictures"
     run "ln -nfs #{shared_path}/config/database.yml #{current_path}/config/database.yml"
   end
-
-  desc "Update Alchemy and generates migrations to finally migrate"
-  task :update, :roles => :app do
-    run "cd #{current_path} && svn update --username #{scm_user} --password #{scm_password} vendor/plugins/alchemy"
-    run "cd #{current_path} && RAILS_ENV=production script/generate plugin_migration"
-    deploy.migrate
-    deploy.restart
-  end
-
+  
   desc "Copies local cache to shared cache folder"
   task :copy_cache, :roles => :app do
     run "mkdir -p #{shared_path}/cache"
@@ -89,16 +49,16 @@ namespace :alchemy do
     run "rm -rf #{current_path}/public/pictures"
     run "ln -nfs #{shared_path}/cache/pictures #{current_path}/public/"
   end
-
+  
   @datestring = Time.now.strftime("%Y_%m_%d_%H_%M_%S")
-
+  
   desc "Get all live data (pictures, attachments and database) from remote server"
   task :get_all_live_data do
     alchemy.get_db_dump
     alchemy.get_pictures
     alchemy.get_attachments
   end
-
+  
   desc "Get all live data (pictures, attachments and database) from remote server and replace the local data with it"
   task :clone_live do
     alchemy.get_all_live_data
@@ -106,65 +66,59 @@ namespace :alchemy do
     alchemy.import_attachments
     alchemy.import_db
   end
-
+  
   desc "Zip all uploaded pictures and store them in shared/uploads folder on server"
   task :zip_pictures do
     run "cd #{deploy_to}/shared/uploads && tar cfz pictures.tar.gz pictures/"
   end
-
+  
   desc "Zip all uploaded attachments and store them in shared/uploads folder on server"
   task :zip_attachments do
     run "cd #{deploy_to}/shared/uploads && tar cfz attachments.tar.gz attachments/"
   end
-
+  
   desc "Make database dump and store into backup folder"
   task :dump_db do
     db_settings = database_settings['production']
     run "cd #{deploy_to}/shared && mysqldump -u#{db_settings['username']} -p#{db_settings['password']} -S#{db_settings['socket']} -h#{db_settings['host']} #{db_settings['database']} > dump_#{@datestring}.sql"
   end
-
+  
   desc "Get pictures zip from remote server and store it in uploads/pictures.tar.gz"
   task :get_pictures do
     alchemy.zip_pictures
     download "#{deploy_to}/shared/uploads/pictures.tar.gz", "uploads/pictures.tar.gz"
   end
-
+  
   desc "Get attachments zip from remote server and store it in uploads/attachments.tar.gz"
   task :get_attachments do
     alchemy.zip_attachments
     download "#{deploy_to}/shared/uploads/attachments.tar.gz", "uploads/attachments.tar.gz"
   end
-
+  
   desc "Get db dump from remote server and store it in db/<Time>.sql"
   task :get_db_dump do
     alchemy.dump_db
     download "#{deploy_to}/shared/dump_#{@datestring}.sql", "db/dump_#{@datestring}.sql"
   end
-
+  
   desc "Extracts the pictures.tar.gz into the uploads/pictures folder"
   task :import_pictures do
     `rm -rf uploads/pictures`
     `cd uploads/ && tar xzf pictures.tar.gz`
   end
-
+  
   desc "Extracts the attachments.tar.gz into the uploads/attachments folder"
   task :import_attachments do
     `rm -rf uploads/attachments`
     `cd uploads/ && tar xzf attachments.tar.gz`
   end
-
+  
   desc "Imports the database file"
   task :import_db do
     db_settings = database_settings['development']
     `rake db:drop`
     `rake db:create`
     `mysql -uroot #{db_settings['database']} < db/dump_#{@datestring}.sql`
-  end
-  
-  desc "PRIVATE! Release a new Alchemy Version. ONLY FOR internal usage!"
-  task :release do
-    system('svn remove -m "removing for new release" http://svn.vondeyen.com/alchemy/releases/v1.1.1')
-    system('svn copy -m "new release" http://svn.vondeyen.com/alchemy/trunk http://svn.vondeyen.com/alchemy/releases/v1.1.1')
   end
   
 end
